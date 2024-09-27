04d0c6f505f52d0c93a527ac44d2bb888c26f871
sunwoo <summerham22@gmail.com>
2024-09-27 14:26:41 +0900

    add os.getenv

--- src/stdash/app.py
+++ src/stdash/app.py
@@ -2,13 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 import streamlit as st
+import os
 
 t1, t2 = st.tabs(['Requests by Date and Time', '불균형요청수'])
 
 #st.title('Requests by Date and Time')
 
 def load_data():
-    url = 'http://43.202.66.118:8077/all'
+    DB = os.getenv('DB')
+    DB_PORT = os.getenv('DB_PORT')
+    url = f'http://{DB}:{DB_PORT}/all'
     r = requests.get(url)
     d = r.json()
     return d
